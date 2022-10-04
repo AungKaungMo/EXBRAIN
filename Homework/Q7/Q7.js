89680a36c4b2ecad61f275c447d626d5af92a63f
AungKaungMoe <moea74759@gmail.com>
2022-10-04 19:31:41 +0630

    Day5 ,6, 7

---
+++ Homework/Q7/Q7.js
@@ -0,0 +1,33 @@
+            //Question1
+var x =103;
+var y =2;
+document.getElementById("a1").innerHTML= x+y;
+document.getElementById("a1").setAttribute("class","p1change");
+
+            //Question2
+var x =34;
+var y =2;
+document.getElementById("a2").innerHTML= x-y;
+document.getElementById("a2").setAttribute("class","p1change");
+
+            //Question3
+var x =3;
+var y =4;
+document.getElementById("a3").innerHTML= x**y;
+document.getElementById("a3").setAttribute("class","p1change");
+
+            //Question4
+var x =3;
+var y =4;
+document.getElementById("a4").innerHTML= x%y;
+document.getElementById("a4").setAttribute("class","p1change");
+
+            //Question5
+var x =5;
+document.getElementById("a5").innerHTML= x++;
+document.getElementById("a5").setAttribute("class","p1change");
+
+            //Question6
+var x =10;
+document.getElementById("a6").innerHTML= --x;
+document.getElementById("a6").setAttribute("class","p1change");
